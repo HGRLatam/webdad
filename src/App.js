b663723479d1d3f4c0da225074683a9e917c91c8
Pedro <hgrlatam@gmail.com>
2022-01-20 14:03:23 -0500

    react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './scss/App.scss';
 
 function App() {
   return (
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mi <code> Primera Web</code> En ReactJs
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://arteddigital.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Diseño&ArteDigital
         </a>
       </header>
     </div>
